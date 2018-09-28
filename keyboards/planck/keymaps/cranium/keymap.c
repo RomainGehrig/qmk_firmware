4944eb9c9a00aead2bd1c9df5f4ce84af930fe96
Romain Gehrig <romain.gehrig@gmail.com>
2018-09-28 10:22:01 +0200

    Add printscr and sleep keys on adjust layer

--- keyboards/planck/keymaps/cranium/keymap.c
+++ keyboards/planck/keymaps/cranium/keymap.c
@@ -103,10 +103,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = LAYOUT_planck_grid(
-    _______, RESET,   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, KC_DEL ,
+    _______, RESET,   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, KC_SLEP,
     _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, KC_VOLU, KC_VOLD,  KC_MPRV, KC_MNXT, _______,
     _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, _______,
-    _______, _______, _______, _______, _______, KC_MPLY, KC_MPLY, _______, _______,  _______, _______, _______
+    _______, _______, _______, _______, _______, KC_MPLY, KC_MPLY, _______, _______,  _______, _______, KC_PSCR
 )
 
 
